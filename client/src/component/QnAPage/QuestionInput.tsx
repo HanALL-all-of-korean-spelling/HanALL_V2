aebdd4e6f3eec00239fd34906e8abc465db83875
seohyun319 <tjguselly0319@gmail.com>
2022-05-15 17:32:35 +0900

    [FEAT] make UI of QnaPage (/qna)

---
+++ client/src/component/QnAPage/QuestionInput.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const QuestionInput = () => {
+  return (
+    <>
+      <input placeholder="제목을 입력해주세요."></input>
+      <textarea placeholder="추가되었으면 하는 내용을 입력해주세요."></textarea>
+      <div>등록하기</div>
+    </>
+  );
+};
