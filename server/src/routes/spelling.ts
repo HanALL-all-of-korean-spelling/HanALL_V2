d0fbd94b4f90208a72035d257fab0d6023230c54
gyeongminK <sally_k@naver.com>
2022-05-19 03:36:07 +0900

    [FIX] flag 기능 수정

--- server/src/routes/spelling.ts
+++ server/src/routes/spelling.ts
@@ -4,8 +4,6 @@ const router = express.Router();
 const esClient = require("../connection.ts");
 const index: String = "words";
 
-let flag: Boolean = false;
-
 //정보글 post 때 넣을 date 값
 let get_today = new Date();
 let get_year = get_today.getFullYear();
@@ -16,10 +14,11 @@ let date = get_year + "-" + get_month + "-" + get_date;
 router
   .route("/")
   .get(async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      let { text, sort_by } = req.query;
-      if (text && !sort_by) {
-        // 철자 정보 검색
+    let { text, sort_by } = req.query;
+    let flag: Boolean = false;
+    if (text && !sort_by) {
+      // 철자 정보 검색
+      try {
         const result = await esClient.search({
           index: index,
           body: {
@@ -27,8 +26,9 @@ router
               bool: {
                 must: [
                   {
-                    match: {
-                      wrong_words: text,
+                    multi_match: {
+                      query: text,
+                      fields: ["right_words", "wrong_words"],
                     },
                   },
                   { match: { type: "spelling" } },
@@ -37,99 +37,93 @@ router
             },
           },
         });
-        console.log("dd", result.body.hits.hits);
-        if (Object.keys(result.body.hits.hits[0]).length != 0) {
-          flag = false; // 틀린 단어 포함 검색
-          return res
-            .status(200)
-            .json({ result: result.body.hits.hits, flag: flag });
-        } else {
-          const result = await esClient.search({
-            index: index,
-            body: {
-              query: {
-                bool: {
-                  must: [
-                    {
-                      match: {
-                        right_words: text,
-                      },
+        const flag_check = await esClient.search({
+          index: index,
+          body: {
+            query: {
+              bool: {
+                must: [
+                  {
+                    match_phrase: {
+                      right_words: text,
                     },
-                    { match: { type: "spelling" } },
-                  ],
-                },
+                  },
+                  { match: { type: "spelling" } },
+                ],
               },
             },
-          });
+          },
+        });
+        if (flag_check.body.hits.total.value > 0) {
           flag = true;
-          return res
-            .status(200)
-            .json({ result: result.body.hits.hits, flag: flag });
         }
-      } else if (sort_by && !text) {
-        // 철자 정보 게시판 조회
-        try {
-          const result = await esClient.search({
-            index: index,
-            sort: [`${sort_by}:desc`],
-            body: {
-              _source: ["title", "hits", "scraps", "created_at"],
-              size: "10",
-              query: {
-                match: {
-                  type: "spelling",
-                },
-              },
-            },
-          });
-          res.status(200).json(result.body.hits.hits);
-        } catch (err) {
-          console.error(err);
-          next(err);
-        }
-      } else if (!text && !sort_by) {
-        // 메인 페이지 요청
-        try {
-          const sort_hits_result = await esClient.search({
-            index: index,
-            body: {
-              _source: ["title", "hits"],
-              sort: { hits: "desc" },
-              size: "3",
-              query: {
-                match: {
-                  type: "spelling",
-                },
-              },
-            },
-          });
-          const sort_crt_result = await esClient.search({
-            index: index,
-            body: {
-              _source: ["title", "hits"],
-              sort: { created_at: "desc" },
-              size: "3",
-              query: {
-                match: {
-                  type: "spelling",
-                },
-              },
-            },
-          });
-          const result: Array<JSON> = [];
-          result.push(sort_hits_result.body.hits.hits);
-          result.push(sort_crt_result.body.hits.hits);
-          res.status(200).json(result);
-        } catch (err) {
-          console.error(err);
-          next(err);
-        }
-      } else {
-        res.status(400).send("error");
+        return res
+          .status(200)
+          .json({ result: result.body.hits.hits, flag: flag });
+      } catch (err) {
+        console.error(err);
+        next(err);
       }
-    } catch (err) {
-      console.error(err);
-      next(err);
+    } else if (sort_by && !text) {
+      // 철자 정보 게시판 조회
+      try {
+        const result = await esClient.search({
+          index: index,
+          sort: [`${sort_by}:desc`],
+          body: {
+            _source: ["title", "hits", "scraps", "created_at"],
+            size: "10",
+            query: {
+              match: {
+                type: "spelling",
+              },
+            },
+          },
+        });
+        res.status(200).json(result.body.hits.hits);
+      } catch (err) {
+        console.error(err);
+        next(err);
+      }
+    } else if (!text && !sort_by) {
+      // 메인 페이지 요청
+      try {
+        const sort_hits_result = await esClient.search({
+          index: index,
+          body: {
+            _source: ["title", "hits", "created_at"],
+            sort: { hits: "desc" },
+            size: "3",
+            query: {
+              match: {
+                type: "spelling",
+              },
+            },
+          },
+        });
+        const sort_crt_result = await esClient.search({
+          index: index,
+          body: {
+            _source: ["title", "hits", "created_at"],
+            sort: { created_at: "desc" },
+            size: "3",
+            query: {
+              match: {
+                type: "spelling",
+              },
+            },
+          },
+        });
+        const result: Array<JSON> = [];
+        result.push(sort_hits_result.body.hits.hits);
+        result.push(sort_crt_result.body.hits.hits);
+        res.status(200).json(result);
+      } catch (err) {
+        console.error(err);
+        next(err);
+      }
+    } else {
+      res.status(400).send("error");
     }
   });
 
