46ab75d93deed20f9d7c0a185e4c06542556a399
seohyun319 <tjguselly0319@gmail.com>
2022-05-23 19:40:03 +0900

    [FEAT] getData on InfoListPage

--- client/src/component/InfoListPage/InfoListPage.tsx
+++ client/src/component/InfoListPage/InfoListPage.tsx
@@ -1,38 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 export const InfoListPage = () => {
-  const spellings = [
-    {
-      _index: "words",
-      _type: "_doc",
-      _id: "1",
-      _score: 0.53899646,
-      _source: {
-        title: "베개 vs 배게",
-        hit: 124,
-      },
-    },
-    {
-      _index: "words",
-      _type: "_doc",
-      _id: "2",
-      _score: 0.53899646,
-      _source: {
-        title: "왠지 vs 웬지",
-        hit: 14,
-      },
-    },
-    {
-      _index: "words",
-      _type: "_doc",
-      _id: "3",
-      _score: 0.53899646,
-      _source: {
-        title: "웬만하면 vs 왠만하면",
-        hit: 114,
-      },
-    },
-  ];
+  const [spellings, setSpellings] = useState<any[]>([]);
+
+  const getData = async () => {
+    await axios.get("/api/spellings?sort=created_at").then((response) => {
+      console.log(response.data);
+      setSpellings(response.data);
+    });
+  };
+  useEffect(() => {
+    getData();
+  }, []);
 
   const renderSpellings =
     spellings &&
@@ -40,7 +20,8 @@ export const InfoListPage = () => {
       return (
         <div key={spelling._id}>
           <div>{spelling._source.title}</div>
-          <div>{spelling._source.hit}</div>
+          <div>{spelling._source.hits}</div>
+          <div>{spelling._source.created_at}</div>
         </div>
       );
     });
