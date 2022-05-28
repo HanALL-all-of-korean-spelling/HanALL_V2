b5f4866dcfee76e535d3e32ad847a5418b0bd4f4
seohyun319 <tjguselly0319@gmail.com>
2022-05-28 23:58:57 +0900

    [FEAT] getData on DetailPage

--- client/src/component/DetailPage/DetailPage.tsx
+++ client/src/component/DetailPage/DetailPage.tsx
@@ -1,17 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { getSpellingDetail } from "../../services/user-service";
 
 export const DetailPage = () => {
-  const detailInfo = {
-    type: "spelling",
-    hit: 34,
-    scrap: 32,
-    title: "베개 vs 배게",
-    right_words: "베개",
-    wrong_words: ["배게", "배개", "베게"],
-    Description: "베개에 대한 설명",
-    Helpful_info: "쉽게 외우는 방법",
-    Related: "",
+  const [detailInfo, setDetailInfo] = useState<Object>({});
+  let id = "uzEM-oABH_eeTwxVb8dK";
+  const getData = async () => {
+    const detail = await getSpellingDetail(id);
+    console.log(detail);
+    setDetailInfo(detail);
   };
+
+  useEffect(() => {
+    getData();
+  }, [id]);
 
   return (
     <>
@@ -19,20 +20,20 @@ export const DetailPage = () => {
         <div>{detailInfo.title}</div>
         <div>
           <div>조회수</div>
-          <div>{detailInfo.hit}</div>
+          <div>{detailInfo.hits}</div>
         </div>
         <div>
           <div>보관</div>
-          <div>{detailInfo.scrap}</div>
+          <div>{detailInfo.scraps}</div>
         </div>
       </div>
       <div>
         <div>😄 옳은 표현: {detailInfo.right_words}</div>
         <div>🤔 틀린 표현: {detailInfo.wrong_words}</div>
-        <div>{detailInfo.Description}</div>
-        <div>{detailInfo.Helpful_info}</div>
+        <div>{detailInfo.description}</div>
+        <div>{detailInfo.helpful_info}</div>
         <div>보관하기</div>
-        {detailInfo.Related && <div>친구 {detailInfo.Related}</div>}
+        {detailInfo.related && <div>친구 {detailInfo.related}</div>}
       </div>
     </>
   );
--- client/src/services/user-service.tsx
+++ client/src/services/user-service.tsx
@@ -12,10 +12,32 @@ export const getSpacingList = (sort: string) => {
     });
 };
 
+export const getSpacingDetail = (id: string) => {
+  return axios
+    .get("/api/spacings/" + id)
+    .then((response) => {
+      return response.data;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
 // spelling
 export const getSpellingList = (sort: string) => {
   return axios
     .get("/api/spellings?sort=" + sort)
+    .then((response) => {
+      return response.data;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+export const getSpellingDetail = (id: string) => {
+  return axios
+    .get("/api/spellings/" + id)
     .then((response) => {
       return response.data;
     })
