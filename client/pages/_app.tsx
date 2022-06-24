2ee175e6429c6220302509aa2a07565874d4782e
seohyun319 <tjguselly0319@gmail.com>
2022-06-25 07:29:24 +0900

    [DESIGN] set mainContiner, global sess flex

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -6,7 +6,9 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Gnb />
-      <Component {...pageProps} />
+      <div className="container">
+        <Component {...pageProps} />
+      </div>
     </>
   );
 }
