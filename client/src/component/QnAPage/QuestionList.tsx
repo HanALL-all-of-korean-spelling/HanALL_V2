99800fe82558d8be1173605aca0abe4ad47063dc
seohyun319 <tjguselly0319@gmail.com>
2022-07-23 23:10:13 +0900

    [DESIGN] set accordion UI to QuestionList

--- client/src/component/QnAPage/QuestionList.tsx
+++ client/src/component/QnAPage/QuestionList.tsx
@@ -1,13 +1,22 @@
 import React, { useState, useEffect } from "react";
 import { IQDetail, IQuestion } from "../../../types";
 import { getQuestionDetail, getQuestions } from "../../services/qna-service";
-import { ListView } from "../ListView/ListView";
-import { Title } from "../Title/Title";
+
+import Accordion from "@mui/material/Accordion";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 export const QuestionList = () => {
   const [qnaList, setQnaList] = useState<IQuestion>();
   const [qnaDetail, setQnaDetail] = useState<IQDetail>();
   const [id, setId] = useState<string>("");
+  const [expanded, setExpanded] = React.useState<string | false>(false);
+
+  const handleChange =
+    (panel: string) => (event: React.SyntheticEvent, isExpanded: boolean) => {
+      setExpanded(isExpanded ? panel : false);
+    };
 
   const getData = async () => {
     const list = await getQuestions(1);
@@ -24,34 +33,37 @@ export const QuestionList = () => {
     qnaList?.result &&
     qnaList.result.map((qna) => {
       return (
-        <div key={qna._id} onClick={() => setId(qna._id)}>
-          <div>{qna._source.title}</div>
-          <div>{qna._source.created_at.substring(0, 10)}</div>
-        </div>
+        <Accordion
+          key={qna._id}
+          onClick={() => setId(qna._id)}
+          expanded={expanded === qna._id}
+          onChange={handleChange(qna._id)}
+          style={{ minWidth: "20rem" }}
+        >
+          <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+            <div style={{ width: "70%", flexShrink: 0 }}>
+              {qna._source.title}
+            </div>
+            <div>{qna._source.created_at.substring(0, 10)}</div>
+          </AccordionSummary>
+          <AccordionDetails>
+            {qnaDetail && (
+              <div>
+                <div>{qnaDetail._source.question}</div>
+                <div className="flex-between">
+                  <div>{qnaDetail._source.nickname}</div>
+                  <div>{qnaDetail._source.created_at.substring(0, 10)}</div>
+                </div>
+              </div>
+            )}
+          </AccordionDetails>
+        </Accordion>
       );
     });
 
   return (
     <div className="margin-x">
-      <ListView>{renderQna}</ListView>
-      <style jsx>{`
-        .cont {
-          margin: 2rem;
-        }
-        div {
-          padding: 0.2rem;
-        }
-      `}</style>
-      {qnaDetail && (
-        <div key={qnaDetail._id} className="cont">
-          <Title size="mid">{qnaDetail._source.title}</Title>
-          <div>{qnaDetail._source.question}</div>
-          <div className="flex-between">
-            <div>{qnaDetail._source.nickname}</div>
-            <div>{qnaDetail._source.created_at.substring(0, 10)}</div>
-          </div>
-        </div>
-      )}
+      <div>{renderQna}</div>
     </div>
   );
 };
