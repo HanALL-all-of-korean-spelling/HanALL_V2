0834508139cbffb4fde5096fe503d2c1bed8a629
seohyun319 <tjguselly0319@gmail.com>
2022-09-17 23:22:15 +0900

    [FEAT] set Gnb responsive design

--- client/src/component/Gnb/Gnb.tsx
+++ client/src/component/Gnb/Gnb.tsx
@@ -1,24 +1,30 @@
 import Link from "next/link";
+import { useState } from "react";
 import { UserNickname } from "../User/UserNickname";
 import style from "./Gnb.module.scss";
 
 export default function Gnb() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className={style.Gnb}>
-      <div>
-        <Link href="/">
+      <div className={style.menu} onClick={() => setIsOpen(!isOpen)}>
+        {isOpen ? <>메뉴 닫기</> : <>메뉴 열기</>}
+      </div>
+      <div className={`${style.List} ${isOpen ? style.Open : style.Close}`}>
+        <Link href="/" passHref>
           <div>메인</div>
         </Link>
-        <Link href="/spelling">
+        <Link href="/spelling" passHref>
           <div>철자 정보</div>
         </Link>
-        <Link href="/spacing">
+        <Link href="/spacing" passHref>
           <div>띄어쓰기 정보</div>
         </Link>
-        <Link href="/qna">
+        <Link href="/qna" passHref>
           <div>문의 게시판</div>
         </Link>
-        <Link href="/scrap">
+        <Link href="/scrap" passHref>
           <div>보관함</div>
         </Link>
       </div>
