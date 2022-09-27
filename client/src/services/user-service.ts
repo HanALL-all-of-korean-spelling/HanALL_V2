857315c83a1eedb6d10c39da33a3819f8b3fae84
seohyun319 <tjguselly0319@gmail.com>
2022-09-28 06:24:41 +0900

    [REFACTOR] arrange user-service.ts using function

---
+++ client/src/services/user-service.ts
@@ -0,0 +1,141 @@
+import axios from "axios";
+import { InfoInputs } from "../../types";
+
+const baseUrl = "http://hanall.site";
+
+const requestGet = (url: string) => {
+  return axios
+    .get(baseUrl + "/api" + url)
+    .then((response) => {
+      return response.data;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+const requestPost = (url: string, body: any) => {
+  return axios
+    .post("/api" + url, body)
+    .then((response) => {
+      return response.data;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+const requestPut = (url: string, body?: any) => {
+  return axios
+    .put("/api" + url, body)
+    .then((response) => {
+      return response.data;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+const requestDelete = (url: string) => {
+  return axios
+    .delete("/api" + url)
+    .then((response) => {
+      return response.data;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+// spacing
+export const getSpacingList = (sort: string, page: number) => {
+  return requestGet("/spacings?sort=" + sort + "&page=" + page);
+};
+
+export const postSpacing = (inputs: InfoInputs) => {
+  requestPost("/spacings", inputs);
+};
+
+export const getSpacingDetail = (id: string) => {
+  requestGet("/spacings/" + id);
+};
+
+export const updateSpacingDetail = (
+  id: string,
+  title: string,
+  right_words: string,
+  wrong_words: string,
+  helpful_info: string
+) => {
+  const body = {
+    title: title,
+    right_words: right_words,
+    wrong_words: wrong_words,
+    helpful_info: helpful_info,
+  };
+  return requestPut("/spacings/" + id, body);
+};
+
+export const deleteSpacingDetail = (id: string) => {
+  return requestDelete("/spacings/" + id);
+};
+
+export const scrapSpacing = (id: string | string[]) => {
+  return requestPut("/spacings/" + id + "/scraps");
+};
+
+// spelling
+export const getSpellingList = (sort: string, page: number) => {
+  requestGet("/spellings?sort=" + sort + "&page=" + page);
+};
+
+export const postSpelling = (inputs: InfoInputs) => {
+  return requestPost("/spellings", inputs);
+};
+
+export const getSpellingDetail = (id: string | string[]) => {
+  requestGet("/spellings/" + id);
+};
+
+export const updateSpellingDetail = (
+  id: string,
+  title: string,
+  right_words: string,
+  wrong_words: string,
+  helpful_info: string
+) => {
+  const body = {
+    title: title,
+    right_words: right_words,
+    wrong_words: wrong_words,
+    helpful_info: helpful_info,
+  };
+  return requestPut("/spellings/" + id, body);
+};
+
+export const deleteSpellingDetail = (id: string) => {
+  return requestDelete("/spellings/" + id);
+};
+
+export const scrapSpelling = (id: string | string[]) => {
+  return requestPut("/spellings/" + id + "/scraps");
+};
+
+// search
+export const getSearchResult = (searchText: string | string[]) => {
+  return requestGet('/spellings?text="' + searchText + '"');
+};
+
+// main
+export const getMainSpacingList = () => {
+  return requestGet("/spacings");
+};
+
+export const getMainSpellingList = () => {
+  return requestGet("/spellings");
+};
+
+// today
+export const getTodayInfo = () => {
+  return requestGet("/todays");
+};
