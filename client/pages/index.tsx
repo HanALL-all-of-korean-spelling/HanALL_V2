9704745b98675bca3606bb73036fb9f2ed332e6b
seohyun319 <tjguselly0319@gmail.com>
2022-09-29 23:26:05 +0900

    [REFACTOR] apply ServerSideProps on mainPage

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,10 +1,20 @@
-import type { NextPage } from "next";
+import type { GetServerSideProps, NextPage } from "next";
+import {
+  getMainSpacingList,
+  getMainSpellingList,
+  getTodayInfo,
+} from "../src/services/user-service";
+import { IMainList, IToday } from "../types";
 import { MainInfo } from "../src/component/MainPage/MainInfo";
 import { TodaySpelling } from "../src/component/MainPage/TodaySpelling";
 import { SearchBar } from "../src/component/SearchPage/SearchBar";
 import { MWContainer } from "../src/component/MWContainer/MWContainer";
 
-const Home: NextPage = () => {
+const Home: NextPage<{
+  todayInfo: IToday;
+  spellingList: IMainList;
+  spacingList: IMainList;
+}> = ({ todayInfo, spellingList, spacingList }) => {
   return (
     <div>
       <style jsx>{`
@@ -14,11 +24,24 @@ const Home: NextPage = () => {
       `}</style>
       <SearchBar />
       <MWContainer>
-        <TodaySpelling />
-        <MainInfo />
+        <TodaySpelling todayInfo={todayInfo} />
+        <MainInfo spellingList={spellingList} spacingList={spacingList} />
       </MWContainer>
     </div>
   );
 };
 
 export default Home;
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const todayInfo = await getTodayInfo();
+  const spellingList = await getMainSpellingList();
+  const spacingList = await getMainSpacingList();
+  return {
+    props: {
+      todayInfo,
+      spellingList,
+      spacingList,
+    },
+  };
+};
