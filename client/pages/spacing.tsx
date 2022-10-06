755b2adc3174ab7c65205b62bf31c251929c83a2
seohyun319 <tjguselly0319@gmail.com>
2022-10-07 01:48:03 +0900

    [REFACTOR] separate component and apply query on SelectSort

--- client/pages/spacing.tsx
+++ client/pages/spacing.tsx
@@ -7,64 +7,20 @@ import { InfoListPage } from "../src/component/InfoListPage/InfoListPage";
 import { Title } from "../src/component/Title/Title";
 import { PaginationView } from "../src/component/PaginationView/PaginationView";
 import { SearchBar } from "../src/component/SearchPage/SearchBar";
+import { SelectSort } from "../src/component/SelectSort/SelectSort";
 
 const Spacing: NextPage<{ spacings: IPageList }> = ({ spacings }) => {
-  const [sort, setSort] = useState<string>("created_at");
   const router = useRouter();
   const page = router.query.page as string;
-
-  const selectSort = () => {
-    return (
-      <>
-        <style jsx>{`
-          div {
-            display: flex;
-            margin: 5px;
-          }
-        `}</style>
-        <div>
-          <div
-            onClick={() => setSort("created_at")}
-            style={
-              sort == "created_at"
-                ? { fontWeight: "bold" }
-                : { fontWeight: "normal" }
-            }
-          >
-            최신순
-          </div>
-          <div>&nbsp;|</div>
-          <div
-            onClick={() => setSort("hits")}
-            style={
-              sort == "hits" ? { fontWeight: "bold" } : { fontWeight: "normal" }
-            }
-          >
-            조회수순
-          </div>
-          <div>&nbsp;|</div>
-          <div
-            onClick={() => setSort("scraps")}
-            style={
-              sort == "scraps"
-                ? { fontWeight: "bold" }
-                : { fontWeight: "normal" }
-            }
-          >
-            스크랩순
-          </div>
-        </div>
-      </>
-    );
-  };
+  const sort = router.query.sort as string;
 
   return (
     <div>
       <SearchBar />
       <Title>띄어쓰기 정보</Title>
-      {selectSort()}
       {spacings && (
         <>
+          <SelectSort sort={sort} page={+page} />
           <InfoListPage list={spacings.result} type={sort} />
           <PaginationView
             total={spacings.total_page}
--- client/pages/spelling.tsx
+++ client/pages/spelling.tsx
@@ -1,80 +1,33 @@
 import type { GetServerSideProps, NextPage } from "next";
 import { useRouter } from "next/router";
-import React, { useState } from "react";
 import { getSpellingList } from "../src/services/user-service";
 import { IPageList } from "../types";
 import { InfoListPage } from "../src/component/InfoListPage/InfoListPage";
 import { Title } from "../src/component/Title/Title";
 import { PaginationView } from "../src/component/PaginationView/PaginationView";
 import { SearchBar } from "../src/component/SearchPage/SearchBar";
+import { SelectSort } from "../src/component/SelectSort/SelectSort";
 
 const Spelling: NextPage<{ spellings: IPageList }> = ({ spellings }) => {
-  const [sort, setSort] = useState<string>("created_at");
   const router = useRouter();
   const page = router.query.page as string;
-
-  const selectSort = () => {
-    return (
-      <>
-        <style jsx>{`
-          div {
-            display: flex;
-            margin: 5px;
-          }
-        `}</style>
-        <div>
-          <div
-            onClick={() => setSort("created_at")}
-            style={
-              sort == "created_at"
-                ? { fontWeight: "bold" }
-                : { fontWeight: "normal" }
-            }
-          >
-            최신순
-          </div>
-          <div>&nbsp;|</div>
-          <div
-            onClick={() => setSort("hits")}
-            style={
-              sort == "hits" ? { fontWeight: "bold" } : { fontWeight: "normal" }
-            }
-          >
-            조회수순
-          </div>
-          <div>&nbsp;|</div>
-          <div
-            onClick={() => setSort("scraps")}
-            style={
-              sort == "scraps"
-                ? { fontWeight: "bold" }
-                : { fontWeight: "normal" }
-            }
-          >
-            스크랩순
-          </div>
-        </div>
-      </>
-    );
-  };
+  const sort = router.query.sort as string;
 
   return (
     <div>
       <SearchBar />
       <Title>철자 정보</Title>
-      {selectSort()}
-      <>
-        {spellings && (
-          <>
-            <InfoListPage list={spellings.result} type={sort} />
-            <PaginationView
-              total={spellings.total_page}
-              current={+page}
-              sort={sort}
-            />
-          </>
-        )}
-      </>
+      {spellings && (
+        <>
+          <SelectSort sort={sort} page={+page} />
+          <InfoListPage list={spellings.result} type={sort} />
+          <PaginationView
+            total={spellings.total_page}
+            current={+page}
+            sort={sort}
+          />
+        </>
+      )}
     </div>
   );
 };
