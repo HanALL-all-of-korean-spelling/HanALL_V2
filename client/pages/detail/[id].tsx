dc3af14853aff5cbd5682a376926ba5ef22f5a15
seohyun319 <tjguselly0319@gmail.com>
2022-10-13 20:20:08 +0900

    [FIX] change ssg to ssr on detailPage

--- client/pages/detail/[id].tsx
+++ client/pages/detail/[id].tsx
@@ -1,10 +1,6 @@
-import type { GetStaticPaths, GetStaticProps, NextPage } from "next";
-import {
-  getSpacingList,
-  getSpellingDetail,
-  getSpellingList,
-} from "../../src/services/user-service";
-import { IDetail, IPageList } from "../../types";
+import type { GetServerSideProps, NextPage } from "next";
+import { getSpellingDetail } from "../../src/services/user-service";
+import { IDetail } from "../../types";
 import { DetailPage } from "../../src/component/DetailPage/DetailPage";
 
 const Detail: NextPage<{ detailInfo: IDetail }> = ({ detailInfo }) => {
@@ -13,24 +9,7 @@ const Detail: NextPage<{ detailInfo: IDetail }> = ({ detailInfo }) => {
 
 export default Detail;
 
-export const getStaticPaths: GetStaticPaths = async () => {
-  const spacings: IPageList = await getSpacingList("hits", 1);
-  const spellings: IPageList = await getSpellingList("hits", 1);
-
-  const spacingPaths = spacings?.result?.map((spacing) => ({
-    params: { id: spacing._id },
-  }));
-  const spellingPaths = spellings?.result?.map((spelling) => ({
-    params: { id: spelling._id },
-  }));
-
-  return {
-    paths: spacingPaths,
-    fallback: true, // build때 만들어지지 않은 path도 추후 요청 들어오면 만들 것
-  };
-};
-
-export const getStaticProps: GetStaticProps = async (context) => {
+export const getServerSideProps: GetServerSideProps = async (context) => {
   const id = context.params?.id as string;
   const detailInfo = await getSpellingDetail(id);
   return {
