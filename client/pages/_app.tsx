401c69f386ba7acfc1ec644423fc94ea2ed6ed57
seohyun319 <tjguselly0319@gmail.com>
2022-10-17 03:39:23 +0900

    [DESIGN] set favicon & appName

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,14 +1,18 @@
-import "../styles/globals.scss";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import { persistor, store } from "../src/_app/store";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import Gnb from "../src/component/Gnb/Gnb";
+import "../styles/globals.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
+        <Head>
+          <title>hanALL</title>
+        </Head>
         <Gnb />
         <div className="container">
           <Component {...pageProps} />
