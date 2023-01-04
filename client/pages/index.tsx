f559fbcf1c776df7f3dba72467b22a38c554cf64
seohyun319 <tjguselly0319@gmail.com>
2023-01-04 21:17:45 +0900

    [FEAT] set CSR on MainPage

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,4 +1,5 @@
 import type { GetServerSideProps, NextPage } from "next";
+import React, { useEffect, useState } from "react";
 import {
   getMainSpacingList,
   getMainSpellingList,
@@ -10,11 +11,28 @@ import { TodaySpelling } from "../src/component/MainPage/TodaySpelling";
 import { SearchBar } from "../src/component/SearchPage/SearchBar";
 import { MWContainer } from "../src/component/common/MWContainer/MWContainer";
 
-const Home: NextPage<{
-  todayInfo: IToday;
-  spellingList: IMainList;
-  spacingList: IMainList;
-}> = ({ todayInfo, spellingList, spacingList }) => {
+// const Home: NextPage<{
+//   todayInfo: IToday;
+//   spellingList: IMainList;
+//   spacingList: IMainList;
+// }> = ({ todayInfo, spellingList, spacingList }) => {
+const Home = () => {
+  const [todayInfo, setTodayInfo] = useState();
+  const [spellings, setSpellings] = useState<IMainList>();
+  const [spacings, setSpacings] = useState<IMainList>();
+
+  useEffect(() => {
+    const getData = async () => {
+      const today = await getTodayInfo();
+      const spellingList = await getMainSpellingList();
+      const spacingList = await getMainSpacingList();
+      setTodayInfo(today);
+      setSpellings(spellingList);
+      setSpacings(spacingList);
+    };
+    getData();
+  }, []);
+
   return (
     <div>
       <style jsx>{`
@@ -25,7 +43,7 @@ const Home: NextPage<{
       <SearchBar showLogo />
       <MWContainer>
         <TodaySpelling todayInfo={todayInfo} />
-        <MainInfo spellingList={spellingList} spacingList={spacingList} />
+        <MainInfo spellingList={spellings} spacingList={spacings} />
       </MWContainer>
     </div>
   );
@@ -33,15 +51,15 @@ const Home: NextPage<{
 
 export default Home;
 
-export const getServerSideProps: GetServerSideProps = async () => {
-  const todayInfo = await getTodayInfo();
-  const spellingList = await getMainSpellingList();
-  const spacingList = await getMainSpacingList();
-  return {
-    props: {
-      todayInfo,
-      spellingList,
-      spacingList,
-    },
-  };
-};
+// export const getServerSideProps: GetServerSideProps = async () => {
+//   const todayInfo = await getTodayInfo();
+//   const spellingList = await getMainSpellingList();
+//   const spacingList = await getMainSpacingList();
+//   return {
+//     props: {
+//       todayInfo,
+//       spellingList,
+//       spacingList,
+//     },
+//   };
+// };
