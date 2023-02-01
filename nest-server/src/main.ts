5f2c48f20f59789c8fad1dce78231bdfe23b42ae
gyeongminK <sally_k@naver.com>
2023-02-01 22:09:53 +0900

    [FEAT] swagger setting

--- nest-server/src/main.ts
+++ nest-server/src/main.ts
@@ -1,8 +1,35 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Swagger Setting
+  const config = new DocumentBuilder()
+    .setTitle('HanALL API Docs')
+    .setVersion('2.0.0')
+    .addBearerAuth(
+      {
+        type: 'apiKey',
+        name: 'accesstoken',
+        in: 'cookie',
+      },
+      'accesstokenAuth',
+    )
+    .addBearerAuth(
+      {
+        type: 'apiKey',
+        name: 'refreshtoken',
+        in: 'cookie',
+      },
+      'refreshtokenAuth',
+    )
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup(`${process.env.SWAGGER_URL}/api-docs`, app, document);
+
   await app.listen(process.env.PORT);
 }
+
 bootstrap();
