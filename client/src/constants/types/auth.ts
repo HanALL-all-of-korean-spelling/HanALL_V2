7195a9373f8ae84d11d75844987f32df2874d00f
seohyun319 <tjguselly0319@gmail.com>
2023-05-15 22:50:35 +0900

    [feat] set query auth

---
+++ client/src/constants/types/auth.ts
@@ -0,0 +1,4 @@
+export interface LoginInputs {
+  email: string;
+  passwd: string;
+}
---
+++ client/src/reactQuery/mutations/auth.ts
@@ -0,0 +1,19 @@
+import { useCallback } from 'react';
+import { useMutation } from '@tanstack/react-query';
+import { LoginInputs } from '@/constants/types';
+import { api } from '../apiClient';
+
+export const useLogin = () => {
+  const loginMutation = useCallback((inputs: LoginInputs) => {
+    return api.post('/auth/login', inputs);
+  }, []);
+
+  const { mutate, data } = useMutation(loginMutation, {
+    onSuccess: (response) => {
+      // TODO: refresh token
+      const token = response.data.accesstoken;
+      api.defaults.headers.common['accesstoken'] = token;
+    },
+  });
+  return { login: mutate, loginResult: data?.data };
+};
