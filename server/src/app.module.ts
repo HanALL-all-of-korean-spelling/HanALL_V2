e4d73c938d876dcfe472c05671db4af6649e594e
gyeongminK <sally_k@naver.com>
2024-01-15 21:27:51 +0900

    [fix] mysql port 수정

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -26,7 +26,7 @@ import { ElasticsearchModule } from '@nestjs/elasticsearch';
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: process.env.MYSQL_HOST,
-      port: 3307,
+      port: 3306,
       username: process.env.MYSQL_USER,
       password: process.env.MYSQL_PASSWORD,
       database: process.env.MYSQL_DATABASE,
